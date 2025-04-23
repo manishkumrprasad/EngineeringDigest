3a722767c72cbc04610a29ccb1307283edf42de2
Manish Kumar Prasad <mkak.2530@gmail.com>
2025-04-23 19:25:00 +0530

    Added Some Code

--- EngineeringDigest/CoreJava/MultiThreading/Revision.java
+++ EngineeringDigest/CoreJava/MultiThreading/Revision.java
@@ -1,4 +1,34 @@
 package EngineeringDigest.CoreJava.MultiThreading;
 
-public class Revision {
+public class Revision extends Thread{
+
+    public static void main(String[] args) {
+        World_Revision w1 = new World_Revision();
+        w1.start();
+
+
+        Boolean run = Boolean.TRUE ;
+        while (run){
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+
+}
+
+//class Test{
+//    public static void main(String[] args) {
+//        System.out.println(Thread.currentThread().getName());
+//    }
+//}
+
+class World_Revision extends Thread{
+
+    @Override
+    public void run() {
+        //System.out.println("The Run Method Is Running");
+        for( ; ;){
+            System.out.println(Thread.currentThread().getName());
+        }
+
+    }
 }
