31fccf54eb2c22b6240dbc38582f5d4bebf33c3f
Manish Kumar Prasad <mkak.2530@gmail.com>
2025-04-23 19:46:27 +0530

    added

--- EngineeringDigest/CoreJava/MultiThreading/Revision_LevelTwo.java
+++ EngineeringDigest/CoreJava/MultiThreading/Revision_LevelTwo.java
@@ -1,4 +1,30 @@
 package EngineeringDigest.CoreJava.MultiThreading;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Revision_LevelTwo {
+
+    public static void main(String[] args) {
+        World_Runnable w1 = new World_Runnable(); //NEW State
+        System.out.println(Thread.currentThread().getState());
+
+        w1.run(); //Runnable State
+        System.out.println(Thread.currentThread().getState());
+    }
+
+
+}
+
+class World_LevelTwo extends  Thread{
+    @Override
+    public void run() {
+        System.out.println("My State Is = " + Thread.currentThread().getState());
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
 }
