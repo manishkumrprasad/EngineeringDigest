dc5abc364968282f94aff6ed392afa3551d5280c
Manish Kumar Prasad <mkak.2530@gmail.com>
2025-04-23 20:08:42 +0530

    enough For today

--- EngineeringDigest/CoreJava/MultiThreading/Revision_LevelTwo.java
+++ EngineeringDigest/CoreJava/MultiThreading/Revision_LevelTwo.java
@@ -5,11 +5,11 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Revision_LevelTwo {
 
     public static void main(String[] args) {
-        World_Runnable w1 = new World_Runnable(); //NEW State
-        System.out.println(Thread.currentThread().getState());
+        World_LevelTwo w1 = new World_LevelTwo(); //NEW State
+        System.out.println(w1.getState());
 
-        w1.run(); //Runnable State
-        System.out.println(Thread.currentThread().getState());
+        w1.start(); //Runnable State
+        System.out.println(w1.getState());
     }
 
 
@@ -18,7 +18,7 @@ public class Revision_LevelTwo {
 class World_LevelTwo extends  Thread{
     @Override
     public void run() {
-        System.out.println("My State Is = " + Thread.currentThread().getState());
+        //System.out.println("My State Is = " + Thread.currentThread().getState());
     }
 
     public static void main(String[] args) {
---
+++ EngineeringDigest/CoreJava/MultiThreading/Revision_MyThread.java
@@ -0,0 +1,31 @@
+package EngineeringDigest.CoreJava.MultiThreading;
+
+public class Revision_MyThread extends Thread{
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Revision_MyThread my1 = new Revision_MyThread();
+        System.out.println(my1.getState());
+
+        my1.start();
+        System.out.println("State After Calling Start Method = " + my1.getState());
+
+        //Make Main Method Sleep For Some Seconds
+        Thread.sleep(1000);
+        System.out.println(my1.getState());
+
+        my1.join(); //whatever code written after this will be executed after the thread-0 terminate
+        System.out.println("running after thread-0 terminates");
+
+
+
+    }
+}
