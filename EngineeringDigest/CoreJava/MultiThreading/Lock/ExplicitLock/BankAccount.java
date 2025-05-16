65c613bf2780018a0e2c6fe6db8c51fbd1f1da4f
Manish Kumar Prasad <mkak.2530@gmail.com>
2025-05-16 23:06:28 +0530

    Explicit Lock

--- EngineeringDigest/CoreJava/MultiThreading/Lock/ExplicitLock/BankAccount.java
+++ EngineeringDigest/CoreJava/MultiThreading/Lock/ExplicitLock/BankAccount.java
@@ -1,22 +1,43 @@
 package EngineeringDigest.CoreJava.MultiThreading.Lock.ExplicitLock;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount extends Thread {
 
     private int balance = 100 ;
 
+
+    private final Lock lock = new ReentrantLock();
+
     public synchronized void withdraw(int amount){
         System.out.println(Thread.currentThread().getName() + " Attempting To Withdraw " +amount);
 
-        if(this.balance >= amount){
-            System.out.println(Thread.currentThread().getName() + " Processing With The Withdraw");
-            try{
-                Thread.sleep(5000);
-            }catch(InterruptedException e){
+        try{
+            if(lock.tryLock(1000 , TimeUnit.MILLISECONDS)){
+
+                if(this.balance >= amount){
+
+                    try{
+                        System.out.println(Thread.currentThread().getName() +" Processing Your Payment");
+                        Thread.sleep(2000);
+                        this.balance -= amount ;
+                        System.out.println(Thread.currentThread().getName() + " Money Withdraw Successfully");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }finally {
+                        lock.unlock();
+                    }
+
+                   }else{
+                    System.out.println(Thread.currentThread().getName() + " Something Went Wrong");
+                }
+            }else{
+                System.out.println(Thread.currentThread().getName() + " could not acquire the lock, will try later on");
             }
-            this.balance -= amount;
-            System.out.println(Thread.currentThread().getName()+ " Withdraw Completed, Remaining Balance In Account : " + this.balance);
-        }else{
-            System.out.println(Thread.currentThread().getName() + " Insufficient Balance In Your Account");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
     }
