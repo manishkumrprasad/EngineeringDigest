b214858e0cbe28da45a52dc202211ac153806e0d
Manish Kumar Prasad <mkak.2530@gmail.com>
2025-06-28 15:13:15 +0530

    Logic-ExceptionHandling

---
+++ EngineeringDigest/ExceptionHandling/Logic.java
@@ -0,0 +1,18 @@
+package EngineeringDigest.BasicJava.Switch;
+
+public class Logic extends Thread{
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread();
+
+        try{
+            t1.sleep(2000); //sleep method throws Interrupted Exception in its method declaration
+            //that's why I have to write the sleep method inside the try-catch block
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
